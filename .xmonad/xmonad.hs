75f0371db4f597dd88114cdcb60541b0ba2027ff
montag451 <montag451@laposte.net>
2016-07-20 10:59:41 +0200

    Modify the configuration of XMonad (add xmobar)

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -1,23 +1,27 @@
-import XMonad
 import qualified Data.Map as M
 
---defaultConfigKeys = keys defaultConfig
+import XMonad
+import XMonad.Hooks.ManageDocks
+import XMonad.Hooks.DynamicLog
+import XMonad.Util.Run
 
 myKeys :: XConfig Layout -> M.Map (KeyMask, KeySym) (X ())
-myKeys conf@(XConfig{modMask = modMask}) = M.union myBindings defaultBindings
+myKeys conf@(XConfig { modMask = modMask }) = M.union myBindings defaultBindings
   where
-    defaultBindings = keys defaultConfig conf
-    myBindings =
-        M.fromList
-            [ ((modMask, xK_semicolon), sendMessage (IncMasterN (-1)))
-            , ((modMask .|. shiftMask, xK_l), spawn "slock")]
+    defaultBindings = keys def conf
+    myBindings = M.fromList [ ((modMask, xK_semicolon), sendMessage (IncMasterN (-1)))
+                            , ((modMask .|. shiftMask, xK_l), spawn "slock")
+                            ]
 
 main :: IO ()
-main =
-    xmonad
-        defaultConfig
-        { modMask = mod4Mask
-        , terminal = "uxterm"
-        , borderWidth = 3
-        , keys = myKeys
-        }
+main = do
+  h <- spawnPipe "xmobar"
+  xmonad def { modMask = mod4Mask
+             , terminal = "uxterm"
+             , borderWidth = 3
+             , keys = myKeys
+             , manageHook = manageHook def <+> manageDocks
+             , layoutHook = avoidStruts $ layoutHook def
+             , handleEventHook = handleEventHook def <+> docksEventHook
+             , logHook = dynamicLogWithPP $ xmobarPP { ppOutput = hPutStrLn h }
+             }
